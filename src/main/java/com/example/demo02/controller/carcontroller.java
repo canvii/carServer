1408bce3e1a744be9903c2e9ac7f89aa68bdc4f4
canvii <1138131867@qq.com>
2021-04-08 00:23:22 +0800

    first

--- src/main/java/com/example/demo02/controller/carcontroller.java
+++ src/main/java/com/example/demo02/controller/carcontroller.java
@@ -1,18 +1,35 @@
 package com.example.demo02.controller;
 
+import com.example.demo02.util.RedisUtil;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.Map;
 
 @Controller
+
 public class carcontroller {
+    @Resource
+    private RedisUtil redisUtil;
     @RequestMapping("/car/{x}/{y}")
-   public String car(@PathVariable(value = "x") String x,@PathVariable(value = "y") String y){
-        System.out.println("X:"+x+"Y:"+y);
+   public String car( @PathVariable(value = "x") String x, @PathVariable(value = "y") String y){
+
+        redisUtil.set("x",x);
+        redisUtil.set("y",y);
+
         return "";
-        
    }
+
+   @RequestMapping("/getPosition")
+    public String getPosition(){
+
+       System.out.println(redisUtil.get("x")+" :"+redisUtil.get("y"));
+
+       return "index1";
+    }
 
 
 
